20207340e4cfc9a930599be465bda238d9778984
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2022-12-10 13:29:27 -0600

    print accounts

--- src/BankApp.java
+++ src/BankApp.java
@@ -1,9 +1,11 @@
+import java.util.LinkedList;
 import java.util.List;
 
 public class BankApp {
     public static void main(String[] args) {
-        String file = "/Users/rodrigomarquez/IdeaProjects/BankApp/src/NewBankAccounts.csv";
-        // Auto generated method stud
+        List<Account> accounts = new LinkedList<Account>();
+
+
 
         /*
         Checking check1 = new Checking("Rodrigo Marquez","32145679",1500);
@@ -15,20 +17,25 @@ public class BankApp {
          */
 
         // Read a CSV File to create new accounts based on data
+        String file = "/Users/rodrigomarquez/IdeaProjects/BankApp/src/NewBankAccounts.csv";
         List<String[]> newAccounts = utilities.CSV.read(file);
-        for (String[] account : newAccounts){
+        for (String[] account : newAccounts) {
             String name = account[0];
             String SSN = account[1];
             String accountType = account[2];
             double initDeposit = Double.parseDouble(account[3]);
-            System.out.println(name + " " + SSN + " " + accountType + " " + initDeposit);
-            if (accountType.equals("Savings")){
-                System.out.println("Open a Savings account");
-            } else if(accountType.equals("Checking")) {
-                System.out.println("Open a Checking account");
+            if (accountType.equals("Savings")) {
+                accounts.add(new Savings(name, SSN, initDeposit));
+            } else if (accountType.equals("Checking")) {
+                accounts.add(new Checking(name, SSN, initDeposit));
+
             } else {
                 System.out.println("Error reading account type");
             }
+        }
+        for (Account acc : accounts) {
+            System.out.println("\n***************");
+            acc.showInfo();
         }
     }
 }
